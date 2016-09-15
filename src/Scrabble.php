218bbe7aea57b729b6b1ea0ba890e091ab2aab66
Anand Angalig and Meredith Alcorn <meredith@>
2016-09-15 10:41:21 -0400

    add all apssings tests for 1 letter inputs

--- src/Scrabble.php
+++ src/Scrabble.php
@@ -32,18 +32,55 @@
         function total_score()
         {
             $one_point_letters = array("A", "E", "I", "O", "U", "L", "N", "R", "S", "T");
+            $two_point_letters = array("D", "G");
+            $three_point_letters = array("B", "C", "M", "P");
+            $four_point_letters = array("F", "H", "V", "W", "Y");
+            $five_point_letters = array("K");
+            $eight_point_letters = array("J", "X");
+            $ten_point_letters = array("Q", "Z");
             $total_points = 0;
+
             foreach ($one_point_letters as $letter) {
                 if($this->input === $letter) {
                     $total_points = $total_points + 1;
                 }
             }
+
+            foreach ($two_point_letters as $letter) {
+                if($this->input === $letter) {
+                    $total_points = $total_points + 2;
+                }
+            }
+
+            foreach ($three_point_letters as $letter) {
+                if($this->input === $letter) {
+                    $total_points = $total_points + 3;
+                }
+            }
+            foreach ($four_point_letters as $letter) {
+                if($this->input === $letter) {
+                    $total_points = $total_points + 4;
+                }
+            }
+            foreach ($five_point_letters as $letter) {
+                if($this->input === $letter) {
+                    $total_points = $total_points + 5;
+                }
+            }
+            foreach ($eight_point_letters as $letter) {
+                if($this->input === $letter) {
+                    $total_points = $total_points + 8;
+                }
+            }
+            foreach ($ten_point_letters as $letter) {
+                if($this->input === $letter) {
+                    $total_points = $total_points + 10;
+                }
+            }
             return $total_points;
+
+
+        }
+
     }
-
-  }
-
-
-
-
- ?>
+?>
--- tests/scrabbleTest.php
+++ tests/scrabbleTest.php
@@ -18,6 +18,92 @@
 
     }
 
+    function test_two_point_letters()
+    {
+        //Arrange
+        $user_input = "D";
+        $test_Scrabble = new Scrabble($user_input);
+
+        //Act
+        $result = $test_Scrabble->total_score();
+        $desired_result = 2;
+
+        //Assert
+        $this->assertEquals($desired_result, $result);
+
+    }
+
+    function test_three_point_letters()
+    {
+        //Arrange
+        $user_input = "B";
+        $test_Scrabble = new Scrabble($user_input);
+
+        //Act
+        $result = $test_Scrabble->total_score();
+        $desired_result = 3;
+
+        //Assert
+        $this->assertEquals($desired_result, $result);
+
+    }
+    function test_four_point_letters()
+    {
+        //Arrange
+        $user_input = "F";
+        $test_Scrabble = new Scrabble($user_input);
+
+        //Act
+        $result = $test_Scrabble->total_score();
+        $desired_result = 4;
+
+        //Assert
+        $this->assertEquals($desired_result, $result);
+
+    }
+    function test_five_point_letters()
+    {
+        //Arrange
+        $user_input = "K";
+        $test_Scrabble = new Scrabble($user_input);
+
+        //Act
+        $result = $test_Scrabble->total_score();
+        $desired_result = 5;
+
+        //Assert
+        $this->assertEquals($desired_result, $result);
+
+    }
+    function test_eight_point_letters()
+    {
+        //Arrange
+        $user_input = "J";
+        $test_Scrabble = new Scrabble($user_input);
+
+        //Act
+        $result = $test_Scrabble->total_score();
+        $desired_result = 8;
+
+        //Assert
+        $this->assertEquals($desired_result, $result);
+
+    }
+    function test_ten_point_letters()
+    {
+        //Arrange
+        $user_input = "Q";
+        $test_Scrabble = new Scrabble($user_input);
+
+        //Act
+        $result = $test_Scrabble->total_score();
+        $desired_result = 10;
+
+        //Assert
+        $this->assertEquals($desired_result, $result);
+
+    }
+
   }
 
 
